e2a446ad6d48120c146278f366e712c26b3d3458
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2024-06-21 14:24:41 +0900

    Adjust for local environment.

--- firmware/spdif/system.h
+++ firmware/spdif/system.h
@@ -2,8 +2,8 @@
 #define __SYSTEM_H__
 
 // Compile switch
-#define UART_EBG_EN             (1)     // 有効にするとちょい重たい
-#define FCS_DMA_EN              (1)     // FCSの計算にDMAを使用する
+#define UART_EBG_EN             (0)     // 有効にするとちょい重たい
+#define FCS_DMA_EN              (0)     // FCSの計算にDMAを使用する
 #define DEF_10BASET_FULL_EN     (1)     // Enable 10BASE-T Full Duplex
 
 
@@ -17,7 +17,7 @@
 
 
 // For UDP
-#define DEF_SYS_UDP_DST_MAC     (0xFFFFFFFFFFFF)
+#define DEF_SYS_UDP_DST_MAC     (0x6C4B909F321F)
 
 #define DEF_SYS_UDP_DST_IP1     (10)
 #define DEF_SYS_UDP_DST_IP2     (253)
